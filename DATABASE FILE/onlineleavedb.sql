d58c436f898bead3a940d3fbfb4e0dfd83ff00f8
Chaitanya1119 <112636187+Chaitanya1119@users.noreply.github.com>
2024-01-23 16:10:00 +0530

    Update onlineleavedb.sql

--- DATABASE FILE/onlineleavedb.sql
+++ DATABASE FILE/onlineleavedb.sql
@@ -1,30 +1,6 @@
--- phpMyAdmin SQL Dump
--- version 4.6.5.2
--- https://www.phpmyadmin.net/
---
--- Host: 127.0.0.1
--- Generation Time: Jul 25, 2021 at 04:49 PM
--- Server version: 5.6.21
--- PHP Version: 5.6.3
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET time_zone = "+00:00";
-
-
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8mb4 */;
-
---
--- Database: `onlineleavedb`
---
-
--- --------------------------------------------------------
-
---
--- Table structure for table `leaves`
---
 
 CREATE TABLE `leaves` (
   `id` int(11) NOT NULL,
@@ -36,25 +12,6 @@ CREATE TABLE `leaves` (
   `status` varchar(255) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
---
--- Dumping data for table `leaves`
---
-
--- INSERT INTO `leaves` (`id`, `eid`, `ename`, `descr`, `fromdate`, `todate`, `status`) VALUES
--- (1, 2, 'williams', 'Other : Demo Test', '2021-07-01', '2021-07-02', 'Rejected'),
--- (2, 2, 'williams', 'Sabbatical : Testing after setting up dates', '2021-07-25', '2021-07-27', 'Pending'),
--- (3, 4, 'elissa', 'Sick : Won\'t be able to join as I am not feeling well enough! Need to take some rest.', '2021-07-25', '2021-07-27', 'Pending'),
--- (4, 2, 'williams', 'Vacation : test after setting up interval days', '2021-07-25', '2021-08-04', 'Pending'),
--- (5, 5, 'gordon', 'Casual : need to spend some time with my family!', '2021-07-26', '2021-07-28', 'Pending'),
--- (6, 6, 'edith', 'Sick : I need to Quarantine myself!', '2021-07-26', '2021-08-09', 'Accepted'),
--- (7, 5, 'gordon', 'Maternity / Paternity : Need to take care of my newborn', '2021-07-26', '2021-08-02', 'Accepted'),
--- (8, 2, 'williams', 'Casual : going on a vacation', '2021-07-27', '2021-08-03', 'Rejected');
-
--- --------------------------------------------------------
-
---
--- Table structure for table `users`
---
 
 CREATE TABLE `users` (
   `id` int(11) NOT NULL,
@@ -69,43 +26,18 @@ CREATE TABLE `users` (
   `phone` varchar(150) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
---
--- Dumping data for table `users`
---
 
 INSERT INTO `users` (`id`, `fullname`, `name`, `password`, `type`, `email`, `gender`, `city`, `department`, `phone`) VALUES
 (1, 'Chaitanya', 'chaithu', '1234', 'admin', 'pavan@gmail.com', 'Male', 'Hyderabad', 'CSE', '9502504293');
 
---
--- Indexes for dumped tables
---
-
---
--- Indexes for table `leaves`
---
 ALTER TABLE `leaves`
   ADD PRIMARY KEY (`id`);
 
---
--- Indexes for table `users`
---
 ALTER TABLE `users`
   ADD PRIMARY KEY (`id`);
 
---
--- AUTO_INCREMENT for dumped tables
---
-
---
--- AUTO_INCREMENT for table `leaves`
---
 ALTER TABLE `leaves`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
---
--- AUTO_INCREMENT for table `users`
---
+
 ALTER TABLE `users`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
